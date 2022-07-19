c03101b0fdab79f0d2decfeccb1ca6790e4e5aeb
olatunji-bashir <109037675+olatunji-bashir@users.noreply.github.com>
2022-07-19 22:25:55 +0100

    Add files via upload
    learning the methods used to edit a list

---
+++ list.py
@@ -0,0 +1,44 @@
+"""
+my_list = ["banana", "cherry", "apple"]
+print(my_list)
+
+my_list.append("lemon")
+print(my_list)
+
+my_list.insert(1, "blueberry")
+print(my_list)
+
+item = my_list.pop()
+print(item)
+
+item = my_list.remove("cherry")
+print(my_list)
+
+item = my_list.reverse()
+print(my_list)
+
+item = my_list.clear()
+print(my_list)
+
+my_numbers = [1, 9, 5, 6, 3, 2]
+my_numbers.sort()
+
+print(my_numbers)
+"""
+new_list = [0] * 5
+print(new_list)
+
+new_list2 = [1, 2, 3, 4, 5]
+
+join_list = new_list + new_list2
+print(join_list)
+
+
+
+"""
+if "lemon" in my_list:
+    print("yes")
+else:
+    print("no")
+"""
+
